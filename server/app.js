44ee32248eb04bc263886845b30fb8dfdf76207e
Mukesh B <mbhalani3@gmail.com>
2017-03-18 14:09:21 -0400

    created server

---
+++ server/app.js
@@ -0,0 +1,35 @@
+
+console.log('SERVER/app.js: begin of file');
+
+// setup express server
+const express = require('express');
+// database
+// const db = require('./db');
+// router
+// const router = require('./routes');
+
+// Middleware
+const morgan = require('morgan');
+const parser = require('body-parser');
+
+const app = express();
+module.exports.app = app;
+
+// set up port we are listening
+app.set('port', 3000);
+
+// logging and parsing
+app.use(morgan('dev'));
+app.use(parser.json());
+
+// set up our routes
+// app.use('/classes', router);
+
+// serve the client files
+app.use(express.static(__dirname + '/../client'));
+
+// if we are being run directly, run the server.
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log(`SERVER: Listening on port: "${app.get('port')}"`);
+}
