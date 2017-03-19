fc03a25e85b02142d97767081ef80ef9cdce7dbd
Mukesh B <mbhalani3@gmail.com>
2017-03-19 00:10:46 -0400

    working on client/app.js

--- client/src/app.js
+++ client/src/app.js
@@ -2,8 +2,24 @@
 console.log('CLIENT/SRC/app.js: begin of file');
 
 var app = {
+  server: 'http://127.0.0.1:3000/classes/messages',
+  username: 'anonymous',
+  roomname: 'lobby',
+  lastMessageId: 0,
+  friends: {},
+  messages: [],
 
   init: function() {
+    // get username
+    app.username = window.location.search.substr(10);
+
+    // cashe jQuery selectors
+    app.$message = $('#message');
+    app.$chats = $('#chats');
+    app.$roomSelect = $('#roomSelect');
+    app.$send = $('#send');
+
+    // add listeners
 
   }
 };
