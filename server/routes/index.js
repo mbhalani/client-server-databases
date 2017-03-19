6348426c0dbbdbe155cca7b20d78e041925811d7
Mukesh B <mbhalani3@gmail.com>
2017-03-18 22:50:39 -0400

    bug fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,9 @@ const router = require('express').Router();
 
 // connect controller methods to their corresponding routes
 router.get('/messages', controller.messages.get);
-router.post('messages', controller.messages.post);
+router.post('/messages', controller.messages.post);
 
-router.get('/users', controller.messages.get);
-router.post('users', controller.messages.post);
+router.get('/users', controller.users.get);
+router.post('/users', controller.users.post);
 
 module.exports = router;
