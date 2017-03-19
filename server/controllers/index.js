15ac3867e5c943e69781b6e4df12c0cb20424679
Mukesh B <mbhalani3@gmail.com>
2017-03-18 22:48:46 -0400

    implemented controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -15,18 +15,32 @@ module.exports = {
     },
     // a function which handles posting a message to the database
     post: function(request, response) {
-      console.log('CONTROLLERS: msg-POST: ', request);
+      console.log('CONTROLLERS: msg-POST: ', request.body);
+      var data = [request.body.username, request.body.message, request.body.roomname];
+      models.messages.post(data, function(err, results) {
+        if (err) { throw err; }
+        response.sendStatus(201);
+      });
     }
   },
 
   users: {
     // a function which handles a get request for all users
     get: function(request, response) {
-
+      console.log('CONTROLLERS: users-GET: ');
+      models.users.get(function(err, results) {
+        if (err) { throw err; }
+        response.json(results);
+      });
     },
     // a function which handles posting a message to the users
     post: function(request, response) {
-
+      console.log('CONTROLLERS: users-POST: ', request.body);
+      var data = [request.body.username];
+      models.users.post(data, function(err, results) {
+        if (err) { throw err; }
+        response.sendStatus(201);
+      });
     }
   }
 };
