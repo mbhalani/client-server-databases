1124defd6a0395c500551237984b53cedd060213
Mukesh B <mbhalani3@gmail.com>
2017-03-18 22:49:51 -0400

    implemented moel

--- server/models/index.js
+++ server/models/index.js
@@ -10,24 +10,41 @@ module.exports = {
       var queryStr = 'SELECT messages.id, users.username, messages.text, messages.roomname \
                     FROM messages LEFT OUTER JOIN users ON (messages.userid = users.id) order by messages.id desc';
       db.query(queryStr, function(err, results) {
-        console.log(results);
         callback(err, results);
       });
     },
     // post message
     post: function(data, callback) {
+      var queryStr = 'INSERT INTO messages (userid, text, roomname) \
+                            VALUES ((SELECT id FROM users WHERE username = ? limit 1), ?, ?)';
 
+      db.query(queryStr, data, function(err, results) {
+        callback(err, results);
+      });
+
+      // var userid;
+      // var queryStr = 'SELECT id FROM users WHERE username = ?;';
+      // db.query(queryStr, data, function(err, results) {
+      //   userid = results[0].id;
+      //   console.log('xxxx ', err, userid);
+      // });
     }
   },
 
   users: {
     // get all users
     get: function(callback) {
-
+      var queryStr = 'SELECT * FROM users';
+      db.query(queryStr, function(err, results) {
+        callback(err, results);
+      });
     },
     // post user
     post: function(data, callback) {
-
+      var queryStr = 'INSERT INTO users (username) VALUES (?)';
+      db.query(queryStr, data, function(err, results) {
+        callback(err, results);
+      });
     }
   }
 };
